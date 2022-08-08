72ec2a0fd169986a2b0bbf2b77efe3524f568cc9
patrik <pcr_batista@hotmail.com>
2022-08-08 18:19:24 -0300

    feat: implementa controller de login

---
+++ src/controllers/login.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from 'express';
+
+import jwt from 'jsonwebtoken';
+import LoginService from '../services/login';
+
+const SECRET = 'segredoSegredosoToken';
+
+export default class LoginController {
+  loginService = new LoginService();
+
+  constructor() {
+    this.getUser = this.getUser.bind(this);
+  }
+
+  public async getUser(req: Request, res: Response) {
+    const { body: { username, password } } = req;
+    const user = await this.loginService.getUser({ username, password });
+    
+    if (user === undefined) {
+      return res.status(401).json({ error: 'Username or password invalid' });
+    } 
+
+    if (user.password !== password) {
+      return res.status(401).json({ error: 'Username or password invalid' });
+    }
+    const token = jwt.sign({ username, id: user.id }, SECRET);    
+    return res.status(200).json({ token });
+  }
+}
