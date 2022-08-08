3fa367f8175a820a3aa2692f1100aee3bb54da9a
patrik <pcr_batista@hotmail.com>
2022-08-08 19:41:50 -0300

    refactor: altera validacoes de products

--- src/middlewares/productValidate.ts
+++ src/middlewares/productValidate.ts
@@ -5,13 +5,13 @@ export default class ProductValidate {
     const { name } = req.body;
     
     if (!name) {
-      return res.status(400).json({ error: 'Name is required' });
+      return res.status(400).json({ message: '"name" is required' });
     } 
     if (typeof name !== 'string') {
-      return res.status(422).json({ error: 'Name must be a string' });
+      return res.status(422).json({ message: '"name" must be a string' });
     }
     if (name.length < 2) {
-      return res.status(422).json({ error: 'Name must be longer than 2 characters' });
+      return res.status(422).json({ message: '"name" length must be at least 3 characters long' });
     }
     next();
   }
@@ -19,13 +19,14 @@ export default class ProductValidate {
   static amountValidate(req: Request, res: Response, next: NextFunction) {
     const { amount } = req.body;
     if (!amount) {
-      return res.status(400).json({ error: 'Amount is required' });
+      return res.status(400).json({ message: '"amount" is required' });
     } 
     if (typeof (amount) !== 'string') {
-      return res.status(422).json({ error: 'Amount must be a string' });
+      return res.status(422).json({ message: '"amount" must be a string' });
     }
     if (amount.length < 2) {
-      return res.status(422).json({ error: 'Amount must be longer than 2 characters' });
+      return res.status(422)
+        .json({ message: '"amount" length must be at least 3 characters long' });
     }
     next();
   }
@@ -33,14 +34,14 @@ export default class ProductValidate {
   static productValidate(req: Request, res: Response, next: NextFunction) {
     const { products } = req.body;
     if (!products) {
-      return res.status(400).json({ error: 'Products is required' });
+      return res.status(400).json({ message: 'Products is required' });
     }
     if (!(Array.isArray(products))) {
-      return res.status(422).json({ error: 'Products must be an array of numbers' });
+      return res.status(422).json({ message: 'Products must be an array of numbers' });
     }
 
     if (products.length === 0) {
-      return res.status(422).json({ error: 'Products can\'t be empty' });
+      return res.status(422).json({ message: 'Products can\'t be empty' });
     }
     next();
   }
