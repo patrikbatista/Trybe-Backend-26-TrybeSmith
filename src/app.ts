dfb5e02e44547e200dab2f6624f0b171f196e2d8
patrik <pcr_batista@hotmail.com>
2022-08-08 18:29:03 -0300

    feat: implementa as rotas na api

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,16 @@
 import express from 'express';
+import 'express-async-errors';
+
+import routes from './routes';
+import error from './middlewares/error';
 
 const app = express();
 
 app.use(express.json());
 
+app.use(routes.product);
+app.use(routes.user);
+app.use(routes.order);
+app.use(routes.login);
+app.use(error);
 export default app;
