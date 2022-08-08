cf9129257ecb8372734511158921ffeea5c3667a
patrik <pcr_batista@hotmail.com>
2022-08-08 18:23:16 -0300

    feat: implementa index

---
+++ src/routes/index.ts
@@ -0,0 +1,11 @@
+import product from './product';
+import user from './user';
+import order from './order';
+import login from './login';
+
+export default {
+  product,
+  user,
+  order,
+  login,
+};
