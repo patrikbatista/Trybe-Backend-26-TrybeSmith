3479fc9451d4b3bb798c3936e66174366e59f178
patrik <pcr_batista@hotmail.com>
2022-08-08 15:42:28 -0300

    feat: implementa order de product

---
+++ src/models/product.ts
@@ -0,0 +1,27 @@
+import { Pool, ResultSetHeader } from 'mysql2/promise';
+import { Product } from '../interfaces';
+
+export default class ProductModel {
+  private connection: Pool;
+
+  constructor(connection: Pool) {
+    this.connection = connection;
+  }
+
+  public async getAll(): Promise<Product[]> {
+    const result = await this.connection.execute('SELECT * FROM Trybesmith.Products');
+    const [rows] = result;
+    return rows as Product[];
+  }
+
+  public async create(product: Product): Promise<Product> {
+    const { name, amount } = product;
+    const result = await this.connection.execute<ResultSetHeader>(
+      'INSERT INTO Trybesmith.Products (name, amount) VALUES (?,?)',
+      [name, amount],
+    );
+    const [dataInserted] = result;
+    const { insertId } = dataInserted;
+    return { id: insertId, ...product };
+  }
+}
