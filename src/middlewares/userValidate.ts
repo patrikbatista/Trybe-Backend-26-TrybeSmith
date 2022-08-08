92247b5d81153d67b933f5074f2c3f950e3aea85
patrik <pcr_batista@hotmail.com>
2022-08-08 19:33:35 -0300

    refactor: altera user middleware

--- src/middlewares/userValidate.ts
+++ src/middlewares/userValidate.ts
@@ -8,13 +8,13 @@ export default class UserValidate {
     const { username } = req.body;
     
     if (!username) {
-      return res.status(400).json({ error: 'Username is required' });
+      return res.status(400).json({ message: '"username" is required' });
     } 
     if (typeof username !== 'string') {
-      return res.status(422).json({ error: 'Username must be a string' });
+      return res.status(422).json({ message: 'Username must be a string' });
     }
     if (username.length <= 2) {
-      return res.status(422).json({ error: 'Username must be longer than 2 characters' });
+      return res.status(422).json({ message: 'Username must be longer than 2 characters' });
     }
     next();
   }
@@ -22,13 +22,13 @@ export default class UserValidate {
   static classeValidate(req: Request, res: Response, next: NextFunction) {
     const { classe } = req.body;
     if (!classe) {
-      return res.status(400).json({ error: 'Classe is required' });
+      return res.status(400).json({ message: 'Classe is required' });
     } 
     if (typeof (classe) !== 'string') {
-      return res.status(422).json({ error: 'Classe must be a string' });
+      return res.status(422).json({ message: 'Classe must be a string' });
     }
     if (classe.length <= 2) {
-      return res.status(422).json({ error: 'Classe must be longer than 2 characters' });
+      return res.status(422).json({ message: 'Classe must be longer than 2 characters' });
     }
     next();
   }
@@ -37,13 +37,13 @@ export default class UserValidate {
     const { level } = req.body;
     
     if (level === undefined) {
-      return res.status(400).json({ error: 'Level is required' });
+      return res.status(400).json({ message: 'Level is required' });
     } 
     if (typeof level !== 'number') {
-      return res.status(422).json({ error: 'Level must be a number' });
+      return res.status(422).json({ message: 'Level must be a number' });
     }
     if (level <= 0) {
-      return res.status(422).json({ error: 'Level must be greater than 0' });
+      return res.status(422).json({ message: 'Level must be greater than 0' });
     }
     next();
   }
@@ -51,13 +51,13 @@ export default class UserValidate {
   static passwordValidate(req: Request, res: Response, next: NextFunction) {
     const { password } = req.body;
     if (!password) {
-      return res.status(400).json({ error: 'Password is required' });
+      return res.status(400).json({ message: '"password" is required' });
     } 
     if (typeof (password) !== 'string') {
-      return res.status(422).json({ error: 'Password must be a string' });
+      return res.status(422).json({ message: 'Password must be a string' });
     }
     if (password.length < 8) {
-      return res.status(422).json({ error: 'Password must be longer than 7 characters' });
+      return res.status(422).json({ message: 'Password must be longer than 7 characters' });
     }
     next();
   }
@@ -66,14 +66,14 @@ export default class UserValidate {
     const { authorization } = req.headers;
     
     if (!authorization) {
-      return res.status(401).json({ error: 'Token not found' });
+      return res.status(401).json({ message: 'Token not found' });
     }
     try {
       const decoded = jwt.verify(authorization, SECRET);
       req.body.user = decoded;
       return next();
     } catch {
-      return res.status(401).json({ error: 'Invalid token' });
+      return res.status(401).json({ message: 'Invalid token' });
     }
   }
 }
