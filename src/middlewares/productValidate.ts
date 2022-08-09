7363fdac808d513ab9febf74ce9661e2f03dc397
patrik <pcr_batista@hotmail.com>
2022-08-09 14:00:51 -0300

    fix: corrige erro lint

--- src/middlewares/productValidate.ts
+++ src/middlewares/productValidate.ts
@@ -38,11 +38,11 @@ export default class ProductValidate {
       return res.status(400).json({ message: '"productsIds" is required' });
     }
     if (!(Array.isArray(productsIds))) {
-      return res.status(422).json({ message: '\"productsIds\" must be an array' });
+      return res.status(422).json({ message: '"productsIds" must be an array' });
     }
 
     if (productsIds.length === 0) {
-      return res.status(422).json({ message: '\"productsIds\" must include only numbers' });
+      return res.status(422).json({ message: '"productsIds" must include only numbers' });
     }
     next();
   }
